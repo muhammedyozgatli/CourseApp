b18da310c5c0c64c665ebdce3c0dad40b566ad04
Muhammed Yozgatli <myozgatli.1907@gmail.com>
2024-09-30 18:26:51 +0300

    UI Constructor olusturuldu

--- appES5.js
+++ appES5.js
@@ -1,11 +1,34 @@
-function Course(title, instructor, image){       //constructor oluşturma
+//course constructor 
+function Course(title, instructor, image){       
     this.title = title;
     this.instructor = instructor;
     this.image = image;
 }
 
+// UI constructor
+function UI(){
+}
 
+UI.prototype.addCourseToList = function(course){
+    const list = document.getElementById('course-list');
 
+    var html = `
+        <tr>
+            <td><img src="img/${course.image}"/></td>
+            <td>${course.title}</td>
+            <td>${course.instructor}</td>
+            <td><a href='#' class="btn btn-danger btn-sm">Delete</a></td>
+        </tr>
+    `;
+    list.innerHTML += html; 
+}
+
+UI.prototype.clearControls = function(course){
+    
+    const title = document.getElementById('title').value="";
+    const instructor =document.getElementById('instructor').value="";
+    const image = document.getElementById('image').value="";
+}
 
 
 document.getElementById('new-course').addEventListener('submit',
@@ -18,6 +41,14 @@ document.getElementById('new-course').addEventListener('submit',
         //course nesnesi oluşturuluyor
         const course = new Course(title,instructor,image);
 
+        //UI olusturuluyor
+        const ui = new UI();
         
+        //course listesi ekleniyor
+        ui.addCourseToList(course);
+
+        //clear controls
+        ui.clearControls();
+
         e.preventDefault(); //Submit olayını kesmek için
 });
