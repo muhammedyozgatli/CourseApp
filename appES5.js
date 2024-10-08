b8875cd61d2f19fa30e44d648b37c5e83263c5e6
Muhammed Yozgatli <myozgatli.1907@gmail.com>
2024-10-08 19:46:40 +0300

    Listeden eleman silme özelliği eklendi

--- appES5.js
+++ appES5.js
@@ -17,7 +17,7 @@ UI.prototype.addCourseToList = function(course){
             <td><img src="img/${course.image}"/></td>
             <td>${course.title}</td>
             <td>${course.instructor}</td>
-            <td><a href='#' class="btn btn-danger btn-sm">Delete</a></td>
+            <td><a href='#' class="btn btn-danger btn-sm delete">Delete</a></td>
         </tr>
     `;
     list.innerHTML += html; 
@@ -28,6 +28,12 @@ UI.prototype.clearControls = function(course){
     const title = document.getElementById('title').value="";
     const instructor =document.getElementById('instructor').value="";
     const image = document.getElementById('image').value="";
+}
+
+UI.prototype.deleteCourse = function(element){
+    if(element.classList.contains('delete')){
+        element.parentElement.parentElement.remove();
+    }
 }
 
 
@@ -51,4 +57,9 @@ document.getElementById('new-course').addEventListener('submit',
         ui.clearControls();
 
         e.preventDefault(); //Submit olayını kesmek için
+});
+
+document.getElementById('course-list').addEventListener('click',function(e){
+const ui = new UI();
+ui.deleteCourse(e.target);
 });
